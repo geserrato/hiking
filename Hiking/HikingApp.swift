65225f64fd693ca6f3f19776024185490348cd06
Gerardo Estrella <g.serrato41@outlook.com>
2026-07-21 22:05:10 -0600

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Gerardo Estrella on 21/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
